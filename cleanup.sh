40f9fb34e498355931a8bb0be22bf84a6b63bbe0
gogoex <110195520+gogoex@users.noreply.github.com>
2026-05-08 04:42:21 +0900

    add ._* to gitignore

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm ~/.basicswap/litecoin/litecoin.conf
+rm ~/.basicswap/navio/navio.conf
+rm ~/.basicswap/particl/particl.conf
+
+rm -rf ~/.basicswap/litecoin/testnet4/wallet*
+rm -rf ~/.basicswap/navio/testnet6/wallet*
+rm -rf ~/.basicswap/particl/testnet/wallet*
